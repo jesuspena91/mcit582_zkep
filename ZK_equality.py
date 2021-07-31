10634ea76d11536b71504796c71454e3ae3945ff
jesuspena91 <jesusp@seas.upenn.edu>
2021-07-30 20:50:08 -0500

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -12,16 +12,9 @@ def ZK_equality(G,H):
 
     C1 = r1 * G
     C2 = r1 * H + m * G
-    D1 = 1
-    D2 = 1
+    D1 = r2 * G
+    D2 = r2 * H + m * G
     zk_proof = 1
-    # r2 = Secret(utils.get_random_num(bits=128))
-    # m = Secret()
-
-    # C1 = r1 * G
-    # C2 = r1 * H + m * G
-    # D1 = r2 * G
-    # D2 = r2 * H + m * G
 
     #Generate a NIZK proving equality of the plaintexts
     # stmt = DLRep(C1,r1*G) & DLRep(C2,r1*H+m*G) & DLRep(D1,r2*G) & DLRep(D2,r2*H+m*G)
