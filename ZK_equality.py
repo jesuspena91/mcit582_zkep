331fe1bf38e9fc747b9aa43d40247868e3e2d133
jesuspena91 <jesusp@seas.upenn.edu>
2021-07-30 21:00:07 -0500

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -8,7 +8,7 @@ def ZK_equality(G,H):
     # Secret randomizers and secret message
     r1 = Secret(utils.get_random_num(bits=128))
     r2 = Secret(utils.get_random_num(bits=128))
-    m = Secret()
+    m = Secret(utils.get_random_num(bits=128))
 
     C1 = r1 * G
     C2 = r1 * H + m * G
@@ -17,7 +17,7 @@ def ZK_equality(G,H):
 
     #Generate a NIZK proving equality of the plaintexts
     stmt = DLRep(C1,r1*G) & DLRep(C2,r1*H+m*G) & DLRep(D1,r2*G) & DLRep(D2,r2*H+m*G)
-    zk_proof = 1
+    zk_proof = stmt.prove()
 
     #Return two ciphertexts and the proof
     return (C1,C2), (D1,D2), zk_proof
